fc4181bb872dfdf41f6c47bd654f9bcc6e0098ea
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-04 23:43:03 +0300

    feat: add book

--- lib/book.py
+++ lib/book.py
@@ -1,16 +1,21 @@
-title = input("Enter the title of the book: ")
-page_count = int(input("Enter the page count of the book: "))
+# title = input("Enter the title of the book: ")
+# page_count = int(input("Enter the page count of the book: "))
 
 class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-        Book.page_count += 1
-
-    @page_count.setter
-    def page_count(self, value):
-        if value < 0 or not isinstance(value, int):
-            raise ValueError("page_count must be an integer")
+        
+        @page_count.setter
+        def page_count(self, value):
+            if value < 0 or type(value) is not int:
+                raise ValueError("page_count must be an integer")
     
     def turn_page(self):
+        self.page_count += 1
         print("Flipping the page...wow, you read fast!")
+
+
+book1 = Book("Atomic Habits", 320)
+
+print(book1.__dict__)
