8aa4ae13924c4a33c09855de00362c9694b490d8
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-04 23:00:53 +0300

    feat: add book

--- lib/book.py
+++ lib/book.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
+title = input("Enter the title of the book: ")
+page_count = int(input("Enter the page count of the book: "))
 
 class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
+        Book.page_count += 1
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
