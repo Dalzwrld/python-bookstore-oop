020445b6441a68afcf8d0dabea0827226db2bb82
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-04 23:08:21 +0300

    feat: add coffee

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+size = input("Enter the size of the coffee: ")
+price = float(input("Enter the price of the coffee: "))
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
