e816172b794ec570d7d5671257ab3f284b7b446a
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-04 23:05:16 +0300

    feat: add book

--- lib/book.py
+++ lib/book.py
@@ -6,6 +6,11 @@ class Book:
         self.title = title
         self.page_count = page_count
         Book.page_count += 1
+
+    @page_count.setter
+    def page_count(self, value):
+        if value < 0 or not isinstance(value, int):
+            raise ValueError("page_count must be an integer")
     
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
