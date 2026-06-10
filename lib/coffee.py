eea640532ac8f740e2788d7bddbb53c21aecae8a
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-10 10:00:44 +0300

    feat: add coffee

--- lib/coffee.py
+++ lib/coffee.py
@@ -15,7 +15,7 @@ class Coffee:
     @size.setter
     def size(self, value):
         if value not in Coffee.SIZE:
-            raise ValueError(f"size must be {Coffee.SIZE[0]}, {Coffee.SIZE[1]}, or {Coffee.SIZE[2]}")
+            raise ValueError(f"Size must be {Coffee.SIZE[0]}, {Coffee.SIZE[1]}, or {Coffee.SIZE[2]}")
         
     def tip(self):
         print("This coffee is great, here's a tip!")
