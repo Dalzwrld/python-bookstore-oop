3ccce6e8f58f5af2fbae51009da00c03508eef55
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-10 09:42:32 +0300

    feat: add coffee

--- lib/coffee.py
+++ lib/coffee.py
@@ -2,6 +2,8 @@ size = input("Enter the size of the coffee (Small, Medium, Large): ")
 price = float(input("Enter the price of the coffee: "))
 
 class Coffee:
+    SIZE = ["Small", "Medium", "Large"]
+
     def __init__(self, size, price):
         self.size = size
         self.price = price
@@ -12,8 +14,8 @@ class Coffee:
 
     @size.setter
     def size(self, value):
-        if value not in ["Small", "Medium", "Large"]:
-            raise ValueError("size must be 'Small', 'Medium', or 'Large'")
+        if value not in Coffee.SIZE:
+            raise ValueError(f"size must be {Coffee.SIZE[0]}, {Coffee.SIZE[1]}, or {Coffee.SIZE[2]}")
         
     def tip(self, price):
         print("This coffee is great, here's a tip!")
